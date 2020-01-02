8f4cd5d72c3a2a44792dc02babc846b8c0da2307
Liz <misstricky@gmail.com>
2020-01-01 16:27:55 -0800

    add more tests and more cleanup

--- src/reducers.js
+++ src/reducers.js
@@ -1,19 +1,5 @@
 import { SAVE_KEY } from './actions';
 
-// function app(state = [], action) {
-//   switch (action.type) {
-//     case SAVE_KEY:
-//       return [
-//         ...state,
-//         {
-//           key: action.key
-//         }
-//       ]
-//     default:
-//       return state
-//   }
-// }
-
 function app(state = [], action) {
   switch (action.type) {
     case SAVE_KEY:
